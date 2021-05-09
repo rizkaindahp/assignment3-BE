8c8f7f941fb9e0336dd7a3fb0605bbf8dae1278d
Rizka Indah Puspita <rizkaindahpuspita@gmail.com>
2021-05-09 15:08:32 +0700

    auth troubleshooting

--- build/src/controllers/auth_controller.js
+++ build/src/controllers/auth_controller.js
@@ -18,6 +18,15 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -32,24 +41,27 @@ class auth {
         dotenv_1.default.config();
     }
     static register(req, res, next) {
-        const emailValidate = EmailValidator.validate(req.body.email);
-        try {
-            if (emailValidate == true) {
-                const newUser = new User_1.User({
-                    username: req.body.username,
-                    fullname: req.body.fullname,
-                    email: req.body.email,
-                    password: bcryptjs_1.default.hashSync(req.body.password, 10),
-                });
-                res.status(201).json({ msg: 'Success create your account', data: newUser });
+        return __awaiter(this, void 0, void 0, function* () {
+            const emailValidate = EmailValidator.validate(req.body.email);
+            try {
+                if (emailValidate == true) {
+                    const newUser = yield User_1.User.create({
+                        username: req.body.username,
+                        fullname: req.body.fullname,
+                        email: req.body.email,
+                        password: bcryptjs_1.default.hashSync(req.body.password, 10),
+                    });
+                    newUser.save();
+                    res.status(201).json({ msg: 'Success create your account', data: newUser });
+                }
+                else {
+                    throw { name: "Email_Fail" };
+                }
             }
-            else {
-                throw { name: "Email_Fail" };
+            catch (err) {
+                next(err);
             }
-        }
-        catch (err) {
-            next(err);
-        }
+        });
     }
     static login(req, res, next) {
         User_1.User.findOne({ email: req.body.email })
